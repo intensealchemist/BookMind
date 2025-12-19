07d8959ba836612e0540100d9f754b6078c86162
intensealchemist <162778488+intensealchemist@users.noreply.github.com>
2025-12-19 16:50:57 +0530

    chore: Download spaCy en_core_web_sm model during build.

--- build.sh
+++ build.sh
@@ -4,5 +4,8 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.prod.txt --no-cache-dir
 
+# Download spaCy language model
+python -m spacy download en_core_web_sm
+
 python manage.py collectstatic --no-input
 python manage.py migrate
